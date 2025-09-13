c615ec6bd14f34babe19ac6e142e2a2289eacc77
ilbertt <luca.bertelli@dfinity.org>
2025-09-13 19:23:51 +0200

    build: remove unnecessary wasm copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import { tanstackViteConfig } from "@tanstack/config/vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import packageJson from "./package.json";
-
-const CORE_RS_DIR = "core/generate/rs";
-const CORE_RS_DIST_WASM_FILE = `./src/${CORE_RS_DIR}/dist/icp-js-bindgen_bg.wasm`;
-const CORE_RS_DIST_WASM_DECLARATIONS_FILE = `./src/${CORE_RS_DIR}/dist/icp-js-bindgen_bg.wasm.d.ts`;
 
 const config = defineConfig({
   test: {
@@ -17,28 +12,6 @@ const config = defineConfig({
   define: {
     __PKG_VERSION__: JSON.stringify(packageJson.version),
   },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: CORE_RS_DIST_WASM_FILE,
-          dest: `esm/${CORE_RS_DIR}/dist`,
-        },
-        {
-          src: CORE_RS_DIST_WASM_DECLARATIONS_FILE,
-          dest: `esm/${CORE_RS_DIR}/dist`,
-        },
-        {
-          src: CORE_RS_DIST_WASM_FILE,
-          dest: `cjs/${CORE_RS_DIR}/dist`,
-        },
-        {
-          src: CORE_RS_DIST_WASM_DECLARATIONS_FILE,
-          dest: `cjs/${CORE_RS_DIR}/dist`,
-        },
-      ],
-    }),
-  ],
 });
 
 export default mergeConfig(
