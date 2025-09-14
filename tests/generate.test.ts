ba9998a903a515ce0564c122f5be1bbc41cd989d
ilbertt <luca.bertelli@dfinity.org>
2025-09-14 15:01:57 +0200

    test: generate snapshots

--- tests/generate.test.ts
+++ tests/generate.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect, beforeAll, beforeEach, vi } from "vitest";
 import { fs as memFs, vol } from "memfs";
-import fs from "node:fs/promises";
+import { readFile } from "node:fs/promises";
+import { join, resolve } from "node:path";
 import { generate } from "../src/core/generate/index.ts";
 import { testWasmInit } from "./utils/wasm.ts";
 
@@ -11,6 +12,7 @@ vi.mock("node:fs/promises", () => ({
 
 const TESTS_ASSETS_DIR = "./tests/assets";
 const OUTPUT_DIR = "output";
+const SNAPSHOTS_DIR = "./assets/snapshots/generate";
 
 beforeAll(async () => {
   await testWasmInit();
@@ -27,11 +29,72 @@ describe("generate", () => {
 
     await generate({ didFile: helloWorldDidFile, outDir: OUTPUT_DIR });
 
-    expect(await fs.readdir(OUTPUT_DIR)).toEqual([
-      "declarations",
-      "hello_world.d.ts",
-      "hello_world.ts",
-      "index.ts",
-    ]);
+    await expectGeneratedOutput(SNAPSHOTS_DIR, helloWorldServiceName);
+  });
+
+  it("should generate a bindgen with canister env feature", async () => {
+    const helloWorldServiceName = "hello_world";
+    const helloWorldDidFile = `${TESTS_ASSETS_DIR}/${helloWorldServiceName}.did`;
+
+    await generate({
+      didFile: helloWorldDidFile,
+      outDir: OUTPUT_DIR,
+      additionalFeatures: {
+        canisterEnv: {
+          variableNames: ["IC_CANISTER_ID:backend"],
+        },
+      },
+    });
+
+    await expectGeneratedOutput(SNAPSHOTS_DIR, helloWorldServiceName);
+    await expect(
+      await readFileFromOutput("canister-env.d.ts")
+    ).toMatchFileSnapshot(
+      `${SNAPSHOTS_DIR}/${helloWorldServiceName}/canister-env.d.ts.snapshot`
+    );
   });
 });
+
+async function readFileFromOutput(path: string): Promise<string> {
+  return await readFile(resolve(OUTPUT_DIR, path), "utf-8");
+}
+
+async function expectGeneratedOutput(
+  snapshotsDir: string,
+  serviceName: string
+): Promise<void> {
+  const generatedOutputDir = join(snapshotsDir, serviceName);
+  const generatedOutputDeclarationsDir = join(
+    generatedOutputDir,
+    "declarations"
+  );
+
+  const declarationsJs = await readFileFromOutput(
+    `declarations/${serviceName}.did.js`
+  );
+  await expect(declarationsJs).toMatchFileSnapshot(
+    `${generatedOutputDeclarationsDir}/${serviceName}.did.js.snapshot`
+  );
+
+  const declarationsTs = await readFileFromOutput(
+    `declarations/${serviceName}.did.d.ts`
+  );
+  await expect(declarationsTs).toMatchFileSnapshot(
+    `${generatedOutputDeclarationsDir}/${serviceName}.did.d.ts.snapshot`
+  );
+
+  const interfaceTs = await readFileFromOutput(`${serviceName}.d.ts`);
+  await expect(interfaceTs).toMatchFileSnapshot(
+    `${generatedOutputDir}/${serviceName}.d.ts.snapshot`
+  );
+
+  const serviceTs = await readFileFromOutput(`${serviceName}.ts`);
+  await expect(serviceTs).toMatchFileSnapshot(
+    `${generatedOutputDir}/${serviceName}.ts.snapshot`
+  );
+
+  const indexTs = await readFileFromOutput("index.ts");
+  await expect(indexTs).toMatchFileSnapshot(
+    `${generatedOutputDir}/index.ts.snapshot`
+  );
+}
