c5f5984788447e74adec9b3e48edb733ef7b56d7
ilbertt <luca.bertelli@dfinity.org>
2025-09-14 15:12:18 +0200

    test: test cli output

---
+++ tests/cli.test.ts
@@ -0,0 +1,25 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { testWasmInit } from "./utils/wasm.ts";
+import { execSync } from "node:child_process";
+import packageJson from "../package.json";
+
+const BIN_FILE = packageJson.bin["icp-bindgen"];
+
+const SNAPSHOTS_DIR = "./assets/snapshots/cli";
+
+beforeAll(async () => {
+  await testWasmInit();
+});
+
+describe("cli", () => {
+  it("should return help message", async () => {
+    const result = execBin(["--help"]);
+
+    await expect(result).toMatchFileSnapshot(`${SNAPSHOTS_DIR}/help.snapshot`);
+  });
+});
+
+function execBin(args: string[]): string {
+  const output = execSync(`node ${BIN_FILE} ${args.join(" ")}`);
+  return output.toString();
+}
