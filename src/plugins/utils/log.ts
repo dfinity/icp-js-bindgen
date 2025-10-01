4b07b9b027f6c0dc286f90b2c106711371faa106
ilbertt <luca.bertelli@dfinity.org>
2025-10-01 20:47:39 +0200

    fix: print error message in cli

--- src/plugins/utils/log.ts
+++ src/plugins/utils/log.ts
@@ -5,3 +5,7 @@ export function cyan(message: string): string {
 export function green(message: string): string {
   return `\x1b[32m${message}\x1b[0m`;
 }
+
+export function red(message: string): string {
+  return `\x1b[31m${message}\x1b[0m`;
+}
